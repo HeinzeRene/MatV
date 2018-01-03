fad1e3df6d80eef6c46b710e0ba9eee13108e3d0
warlord25081 <34706554+warlord25081@users.noreply.github.com>
2018-01-03 15:52:36 +0100

    update

--- leihVorgang/src/main/java/Projektarbeit/leihVorgang/AngebotErstellen.java
+++ leihVorgang/src/main/java/Projektarbeit/leihVorgang/AngebotErstellen.java
@@ -1,20 +1,80 @@
 package Projektarbeit.leihVorgang;
 
-import org.slf4j.ILoggerFactory;
-import org.slf4j.spi.LoggerFactoryBinder;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.zip.DataFormatException;
 
-public class AngebotErstellen implements LoggerFactoryBinder {
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	@Override
-	public ILoggerFactory getLoggerFactory() {
-		// TODO Auto-generated method stub
-		return null;
+import DataBase.DB_Person;
+
+public class AngebotErstellen  {
+
+
+	private static final Logger L = LoggerFactory.getLogger(DB_Person.class);
+	private int leihscheinNummer = -1;	
+	private int idGremium = -1;
+	
+	private Connection connection;
+		public void setConnection(Connection connection) {
+		this.connection = connection;
+	}//end of connection
+	
+	private Connection getConnection() {
+		if (connection == null) {
+			try {
+				throw new Exception("Connection not set");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}//end of getConnection 
+
+	public int erstelleLeihscheinNummer() {
+		L.info("Start einlesen der letzten vergebenen Leihscheinnummer");
+		String sql = "select max(leihscheinNummer) from leihschein";
+		L.info(sql);
+		try(PreparedStatement s = connection.prepareStatement(sql)){
+			
+			leihscheinNummer = Integer.parseInt(sql) +1;
+			s.executeUpdate();
+		}catch  (SQLException e) {
+			L.error(""+e);
+			try {
+				throw new Exception();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+		L.info("Ende des Einlesens");
+		return leihscheinNummer;
 	}
-
-	@Override
-	public String getLoggerFactoryClassStr() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	public void execute(DelegateExecution execution) throws Exception {
+	
+		leihscheinNummer = erstelleLeihscheinNummer();
+		if(execution.getVariable("idGremium") == "") {
+			
+		}
+		L.info("Start einlesen von Leihscheindaten");
+		String sql = "insert into leihschen(leihscheinNummer, idGremium, anfangausleihe, endeausleihe" + 
+				"values (?, ?, ?, ?)";
+		L.info(sql);
+		try(PreparedStatement s = connection.prepareStatement(sql)){
+			s.setInt(1, leihscheinNummer);
+			s.setInt(2, idGremium);
+			s.setDate(3, anfangausleihe);
+			s.setDate(4, endeausleihe);
+			s.executeUpdate();
+		}catch  (SQLException e) {
+			L.error(""+e);
+			throw new DataFormatException();
+		}
+		L.info("Ende des Einlesens");
+	}// end of execution
 
 }
