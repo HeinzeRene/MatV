5fb47072b82fc626b52b3957e65a65618eb1c9d0
Chris Lohr <33151582+ChrisLohr@users.noreply.github.com>
2018-01-03 13:34:04 +0100

    Utils gelöscht

--- leihVorgang/src/main/java/Projektarbeit/utils/DbCred.java
+++
@@ -1,9 +0,0 @@
-package Projektarbeit.utils;
-
-public interface DbCred {
-	  final String driverClass = "oracle.jdbc.driver.OracleDriver";
-	  final String url = "jdbc:oracle:thin:@oradbs02.f4.htw-berlin.de:1521:orcl";
-	  final String user = "u558270";
-	  final String password = "p558270";
-	  final String schema = "u558270";
-}
