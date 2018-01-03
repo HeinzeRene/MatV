682fa04b6f0db970b4f8104c70ee662ab4082354
MuffinUser <MuffinUser@users.noreply.github.com>
2018-01-03 13:07:35 +0100

    Umbennenung Materiallistezusammenstellen zu StartListenZusammenstellen

--- leihVorgang/src/main/java/Projektarbeit/leihVorgang/StartListenZusammenstellen.java
+++ leihVorgang/src/main/java/Projektarbeit/leihVorgang/StartListenZusammenstellen.java
@@ -3,7 +3,7 @@ package Projektarbeit.leihVorgang;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class MateriallisteZusammenstellen implements JavaDelegate{
+public class StartListenZusammenstellen implements JavaDelegate{
 
 	@Override
 	public void execute(DelegateExecution arg0) throws Exception {
