aea22d4c19691464d53fc700f74767fa3ef5fe5d
warlord25081 <34706554+warlord25081@users.noreply.github.com>
2018-01-03 15:01:38 +0100

    update

---
+++ leihVorgang/src/main/java/Projektarbeit/leihVorgang/AngebotErstellen.java
@@ -0,0 +1,20 @@
+package Projektarbeit.leihVorgang;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
+
+public class AngebotErstellen implements LoggerFactoryBinder {
+
+	@Override
+	public ILoggerFactory getLoggerFactory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLoggerFactoryClassStr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
