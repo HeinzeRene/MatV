feeef0c2d25bb1813e7d6729c0a041d3ef551c4b
Chris Lohr <33151582+ChrisLohr@users.noreply.github.com>
2018-01-03 12:55:05 +0100

    PruefenKundenkonto refreshen

--- leihVorgang/src/main/java/Projektarbeit/leihVorgang/PruefenKundenkonto.java
+++ leihVorgang/src/main/java/Projektarbeit/leihVorgang/PruefenKundenkonto.java
@@ -1,11 +1,11 @@
 package Projektarbeit.leihVorgang;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import de.htwberlin.exceptions.DataException;
-import de.htwberlin.mauterhebung.Fahrzeug;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Logger;
 
 public class PruefenKundenkonto {
 
@@ -14,7 +14,7 @@ public class PruefenKundenkonto {
 	
 	
 	
-	private static final Logger L = LoggerFactory.getLogger(DB_Person.class);
+	private static final Logger L = (Logger) LoggerFactory.getLogger(PruefenKundenkonto.class);
 	
 	private Connection connection;
 		public void setConnection(Connection connection) {
@@ -33,7 +33,7 @@ public class PruefenKundenkonto {
 		return connection;
 	}//end of getConnection
 	
-	private boolean getPerson(String vorname, String nachname) {
+	private boolean getPerson(String vorname, String nachname) throws Exception {
 		
 		
 		L.info("Start Auslesen von Personendaten");
@@ -42,8 +42,8 @@ public class PruefenKundenkonto {
 		String sql = "select * person where vorname = ? and nachname is ?";
 		L.info(sql);
 		try(PreparedStatement ps = connection.prepareStatement(sql)){
-			ps.setString(1, getVariable("vorname"));
-			ps.setString(2, getVariable("nachname"));
+			ps.setString(1, "VORNAME");
+			ps.setString(2, "NACHNAME");
 			try(ResultSet rs = ps.executeQuery()){
 				if(rs.next()) {
 					idPerson = rs.getInt("idPerson");
@@ -57,18 +57,18 @@ public class PruefenKundenkonto {
 				}
 			}catch  (SQLException e) {
 			L.error(""+e);
-			throw new DataException(e);
+			throw new Exception(e);
 			}
 		L.info("Ende des Einlesens");
 
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			
+			e.printStackTrace();
 		}
-		if(eMailAdresse = null) {
+		if(eMailAdresse == null) {
 			
 		}
-		return fzg;	
+		return false;	
 		
 		
 	}
