56169c45ef0b2954385561854fcbcf52042a3546
warlord25081 <34706554+warlord25081@users.noreply.github.com>
2018-01-03 14:25:57 +0100

    update

--- leihVorgang/src/main/java/Projektarbeit/leihVorgang/PruefenKundenkonto.java
+++ leihVorgang/src/main/java/Projektarbeit/leihVorgang/PruefenKundenkonto.java
@@ -9,11 +9,7 @@ import ch.qos.logback.classic.Logger;
 
 public class PruefenKundenkonto {
 
-	int idPerson;
-	String anrede, vorname, nachname, matrikelnummer, adresse, plz, wohnort, eMailAdresse;
-	
-	
-	
+	private String eMailAdresse;
 	private static final Logger L =  (Logger) LoggerFactory.getLogger(PruefenKundenkonto.class);
 	
 	private Connection connection;
@@ -34,25 +30,15 @@ public class PruefenKundenkonto {
 	}//end of getConnection
 	
 	private boolean getPerson(String vorname, String nachname) throws Exception {
-		
-		
-		L.info("Start Auslesen von Personendaten");
-		//Person prs = new Person (null, "", "", "", "", "", "", "", "");
+			
 		L.info("Start Auslesen von Kundendaten");
-		String sql = "select * person where vorname = ? and nachname is ?";
+		String sql = "select * person from where vorname = ? and nachname is ?";
 		L.info(sql);
 		try(PreparedStatement ps = connection.prepareStatement(sql)){
-			ps.setString(1, "VORNAME");
-			ps.setString(2, "NACHNAME");
+			ps.setString(1, "vorname");
+			ps.setString(2, "nachname");
 			try(ResultSet rs = ps.executeQuery()){
 				if(rs.next()) {
-					idPerson = rs.getInt("idPerson");
-					vorname = rs.getString("vorname");
-					nachname = rs.getString("nachname");
-					matrikelnummer = rs.getString("matrikelnummer");
-					adresse = rs.getString("adresse");
-					plz = rs.getString("plz");
-					wohnort = rs.getString("wohnort");
 					eMailAdresse = rs.getString("eMailAdresse");
 				}
 			}catch  (SQLException e) {
@@ -60,17 +46,14 @@ public class PruefenKundenkonto {
 			throw new Exception(e);
 			}
 		L.info("Ende des Einlesens");
-
-		} catch (SQLException e) {
-			
+		} catch (SQLException e) {	
 			e.printStackTrace();
 		}
+		
 		if(eMailAdresse == null) {
-			
-		}
-		return false;	
-		
-		
-	}
-	
-}
+			return false;	
+		}else {
+			return true;
+		}	
+	}//end of getPerson
+}//end of class
